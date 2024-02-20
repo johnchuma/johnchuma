cd22f3a472210f432ea4888cfcae70681fbc6a2d
John Vedastus Chuma <johnvchuma@gmail.com>
2024-02-20 20:07:10 +0300

    saven changes

--- src/app/components/footer.js
+++ src/app/components/footer.js
@@ -4,18 +4,24 @@ const Footer = () => {
     return ( <div>
         <div className="w-full bg-slate-950 py-20 text-white">
         <Experience/>
-        <div className="grid grid-cols-1 md:grid-cols-12  w-11/12 space-y-3 text-center md:text-start justify-center md:justify-start md:w-11/12 mx-auto">
+        <div className="grid grid-cols-1 md:grid-cols-12 items-center   w-11/12 space-y-3 text-center md:text-start justify-center md:justify-start md:w-11/12 mx-auto">
             <div className="col-span-8">
                 <div className="flex flex-col md:flex-row items-center md:items-start space-x-2 space-y-2">
                 <Image src={"/me.jpg"} height={1000} width={1000} className="aspect-squire rounded-full h-20 w-20 md:h-10 md:w-10 object-cover" alt="John Vedastus Chuma"/>
                 <h1 className=" md:text-lg md:block hidden">John Chuma</h1>
                 </div>
                 <p className="text-lg py-4">Subscribe to receive blog updates</p>
-                <div className="flex justify-between pe-4 bg-indigo-700 rounded-lg md:w-4/12">
-                    <input placeholder="Write your email here..." className=" py-4  rounded-s-lg "/>
-                    <button >Subscribe</button>
+                <div className=" grid grid-cols-12 justify-between  items-center bg-white  rounded-lg md:w-4/12">
+                    <div className="col-span-8">
+                    <input placeholder="Write your email here..." className=" py-4 border-0  rounded-s-lg "/>
+                    </div>
+                    <div className="col-span-4">
+                    <button className="bg-indigo-700 py-5 w-full px-3 text-sm rounded-e-lg">Subscribe</button>
+                    </div>
+
+                    
                 </div>
-                <p className="text-xs md:text-sm mt-4">By subscribing you agree to with our Privacy Policy and provide consent to receive updates from our this website</p>
+                <p className="text-xs md:text-sm mt-4 opacity-80">By subscribing you agree to with our Privacy Policy and provide consent to receive updates from our this website</p>
             </div>
             <div className=" col-span-12 md:col-span-2">
             <h2 className="text-white font-bold text-lg pb-4">Address</h2>
--- src/app/components/ms.js
+++ src/app/components/ms.js
@@ -5,13 +5,13 @@ import ProjectList from "./project_list";
 const MS= () => {
     return ( <div>
        <div className="bg-pink-300 py-24 md:text-start text-center ">
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-x-12 w-11/12 mx-auto items-center justify-center">
+        <div className="grid gap-y-4 grid-cols-1 md:grid-cols-2 gap-x-12 w-11/12 mx-auto items-center justify-center">
        
             <Image width={3000} height={3000} src={"/server.avif" }/>
         
         <div>
-           <h1 className="text-4xl font-bold">Management systems</h1>
-           <h1 className="text-lg mt-3  md:pe-32">I have 10+ web projects projects that i have successfully developed and deployed </h1>
+           <h1 className="text-3xl md:text-4xl font-bold">Management systems</h1>
+           <h1 className="text-lg mt-3  md:pe-32">I have 4+ Management systems projects that i have successfully developed and deployed </h1>
            <div className="text-sm flex flex-wrap justify-center md:justify-start mt-10 ">
            <ProjectList category={"MS"}/>
 
